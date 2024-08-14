28548c69fc1fd966341586ffdb67280328d29f03
jkobject <jkobject@gmail.com>
2024-08-14 18:11:16 +0200

    pushing the tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -13,4 +13,8 @@ def test_base():
     random_mask = np.random.choice([0, 1], size=random_matrix.shape, p=[0.8, 0.2])
     sparse_random_matrix = csr_matrix(random_matrix * random_mask)
     grn = GRNAnnData(adata.copy(), grn=sparse_random_matrix)
-    assert True, "GRNAnnData test passed"
+
+    assert isinstance(grn, GRNAnnData), "grn should be an instance of GRNAnnData"
+    assert grn.shape == adata.shape, "grn shape should match adata shape"
+    # assert np.array_equal(grn.X, adata.X), "grn.X should match adata.X"
+    # assert np.array_equal(grn.grn.toarray(), sparse_random_matrix.toarray()), "grn.grn should match the input sparse_random_matrix"
